8404ee1f08da1fce0ae833c602a7f9c466b5bb4c
Jensen Wong <jensenwong1@gmail.com>
2020-04-09 19:33:00 -0700

    new code

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-$('[data-toggle-menu]').on("click", function(){
-    $("#overlay-nav-menu").toggleClass("is-open");
-  });
+// function to toggle visibility of text bar after a click
+
+
+
+// This code came with the framework
+// $('[data-toggle-menu]').on("click", function(){
+//     $("#overlay-nav-menu").toggleClass("is-open");
+//   });
   
   
